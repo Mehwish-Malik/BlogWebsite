069921934ae214133c79c8921c780a52269dd66c
wish Malik <malik.mehwish0078@gmail.com>
2025-01-12 06:09:13 +0500

    first commit

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,8 +3,8 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset:"production",
+  apiVersion: "v2025-01-01",
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
